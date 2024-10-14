9714c414e9029f9b661e70c9db9df57eb035bdd6
TimBoostGraphics <tim@boostgraphics.tv>
2024-10-14 22:07:09 +0200

    feat(main): * added typewriter effect * added start of color

--- chat/central-chat-display/src/App.js
+++ chat/central-chat-display/src/App.js
@@ -1,103 +1,125 @@
-import './App.css';
-import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
+import "./App.css";
+import React, { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
 
-const socket = io('http://localhost:3001');
+const socket = io("http://localhost:3001");
 
 function CentralChatDisplay() {
-    const [messages, setMessages] = useState([]);
-    const [messageBuffer, setMessageBuffer] = useState([]);
-    const [processingMessage, setProcessingMessage] = useState('');
-    const chatBoxRef = useRef(null);  // Reference to the chat box
+  const [messages, setMessages] = useState([]);
+  //const [processingMessage, setProcessingMessage] = useState("");
 
-    useEffect(() => {
-        socket.on('load-messages', (loadedMessages) => {
-            setMessages(loadedMessages);
-        });
+  const processing = useRef(false);
+  const messageBuffer = useRef([]);
+  const chatBoxRef = useRef(null); // Reference to the chat box
 
-        socket.off('chat-message');
-        socket.on('chat-message', (newMessage) => {
-            setMessageBuffer((prevMessages) => {
-                // Check if the message with the same id exists in the array
-                const index = prevMessages.findIndex((msg) => msg.id === newMessage.id);
-                    
-                if (index !== -1) {
-                    // Update the existing message by creating a new array with the updated message
-                    const updatedMessages = [...prevMessages];
-                    updatedMessages[index] = newMessage;
-                    return updatedMessages;
-                } else {
-                    // If message doesn't exist, add it to the array
-                    return [...prevMessages, newMessage];
-                }
-            });
-        });
-    }, []);
+  useEffect(() => {
+    socket.on("load-messages", (loadedMessages) => {
+      setMessages(loadedMessages);
+    });
 
-    // Automatically process buffer when there's a new message
-    useEffect(() => {
-        if (messageBuffer.length > 0 && !processingMessage) {
-            const [currentMessage, ...rest] = messageBuffer;
-            setProcessingMessage(String(currentMessage.message));
-            setMessageBuffer(rest);
+    socket.off("chat-message");
+    socket.on("chat-message", (newMessage) => {
+      // Check if the message with the same id exists in the array
+      const index = messageBuffer.current.findIndex(
+        (msg) => msg.id === newMessage.id
+      );
 
-            var userNameElement = document.createElement('span');
-            userNameElement.className = 'system-text';
-            userNameElement.innerHTML = currentMessage.username + ': ';
+      if (index !== -1) {
+        // Update the existing message by creating a new array with the updated message
+        const updatedMessages = [...messageBuffer.current];
+        updatedMessages[index] = newMessage;
+        messageBuffer.current = updatedMessages;
+      } else {
+        // If message doesn't exist, add it to the array
+        messageBuffer.current = [...messageBuffer.current, newMessage];
+      }
+    });
 
-            var inputElement = document.getElementById('input-message');
-            inputElement.replaceChild(userNameElement, inputElement.firstChild);
+    const interval = setInterval(() => {
+      !processing.current && onProcessingMessage();
+    }, 50);
 
-            typeWriter(0);
+    return () => clearInterval(interval);
+  }, []);
 
-            setProcessingMessage(null);
+  // Automatically process buffer when there's a new message
+  const onProcessingMessage = () => {
+    if (messageBuffer.current.length > 0) {
+      const [currentMessage, ...rest] = messageBuffer.current;
+      messageBuffer.current = rest;
 
-            setMessages((prevMessages) => {
-                // Check if the message with the same id exists in the array
-                const index = prevMessages.findIndex((msg) => msg.id === currentMessage.id);
-        
-                if (index !== -1) {
-                    // Update the existing message by creating a new array with the updated message
-                    const updatedMessages = [...prevMessages];
-                    updatedMessages[index] = currentMessage;
-                    return updatedMessages;
-                } else {
-                    // If message doesn't exist, add it to the array
-                    return [...prevMessages, currentMessage];
-                }
-            });
+      document.getElementById("userName").innerHTML =
+        currentMessage.username + ": ";
+      document.getElementById("message").style = { color: "red" };
+
+      typeWriter(0, currentMessage);
+      processing.current = true;
+    }
+  };
+
+  const typeWriter = (index, currentMessage) => {
+    if (index < currentMessage.message.length) {
+      document.getElementById("message").innerHTML +=
+        currentMessage.message.charAt(index);
+
+      setTimeout(() => {
+        typeWriter(index + 1, currentMessage);
+      }, 200);
+    } else {
+      processing.current = false;
+
+      document.getElementById("userName").innerHTML = "Username: ";
+      document.getElementById("message").innerHTML = "";
+
+      setMessages((prevMessages) => {
+        // Check if the message with the same id exists in the array
+        const index = prevMessages.findIndex(
+          (msg) => msg.id === currentMessage.id
+        );
+
+        if (index !== -1) {
+          // Update the existing message by creating a new array with the updated message
+          const updatedMessages = [...prevMessages];
+          updatedMessages[index] = currentMessage;
+          return updatedMessages;
+        } else {
+          // If message doesn't exist, add it to the array
+          return [...prevMessages, currentMessage];
         }
-    }, [messageBuffer, processingMessage]);
+      });
+    }
+  };
 
-    const typeWriter = (index) => {
-        if (index < processingMessage.length) {
-            document.getElementById('input-message').innerHTML += processingMessage.charAt(index);
-            setTimeout(() => {
-                typeWriter(index + 1);
-            }, 50);
-        }
-    };
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
 
-    useEffect(() => {
-        if (chatBoxRef.current) {
-            chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
-        }
-    }, [messages]);
-
-    return (
-        <div class="chat-container">
-            <div class="chat-messages" id="chat-box" ref={chatBoxRef}>
-                {messages.map((msg, index) => (
-                    <div key={index} class="chat-message">
-                        <span class='system-text'>{msg.username}:</span> {msg.message}
-                    </div>
-                ))}
+  return (
+    <div className="chat-container">
+      <div className="chat-messages" id="chat-box" ref={chatBoxRef}>
+        {messages.map((msg, index) => (
+          <div key={index} className="chat-message">
+            <div className="system-text">{msg.username}: </div>
+            <div
+              style={{ color: msg.message.startsWith("red:") ? "red" : "blue" }}
+            >
+              {msg.message}
             </div>
-            <div class="chat-input-container">
-                <p id='input-message'><span class='system-text'>Username: </span></p>
-            </div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-input-container">
+        <div id="input-message" style={{ display: "flex" }}>
+          <div className="system-text" id="userName">
+            Username:{" "}
+          </div>
+          <div id="message"></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CentralChatDisplay;
