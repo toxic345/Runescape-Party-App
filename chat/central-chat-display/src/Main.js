3e0b46f7a7cb3fe24e2f5d43580ceaab115d6151
toxic345 <mathias.annot@gmail.com>
2024-10-17 17:28:41 +0200

    Update Main.js

--- chat/central-chat-display/src/Main.js
+++ chat/central-chat-display/src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import App from './App';
 import AdminLogin from './AdminLogin';
 import Admin from './Admin';
@@ -7,13 +7,11 @@ import Admin from './Admin';
 function Main() {
     console.log('Main Component Rendered');
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/admin-login" element={<AdminLogin />} />
-                <Route path="/admin" element={<Admin />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
+            <Route path="/admin" element={<Admin />} />
+        </Routes>
     );
 }
 
