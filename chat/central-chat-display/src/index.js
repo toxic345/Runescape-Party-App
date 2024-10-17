d06d2e79c24d7a0d306bc8a72e1ec1e3900ac1d3
toxic345 <mathias.annot@gmail.com>
2024-10-17 12:29:24 +0200

    Update index.js

--- chat/central-chat-display/src/index.js
+++ chat/central-chat-display/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import Main from './Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create root
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Main />
-  </React.StrictMode>
+  </BrowserRouter>
 );
